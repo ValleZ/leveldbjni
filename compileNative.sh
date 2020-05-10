d697e73964fac6de6740479929b1a99478107adc
Valentin Konovalov <valle.ketsujin@gmail.com>
2020-05-10 08:15:57 -0400

    move external files out

--- compileNative.sh
+++ compileNative.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export set JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home/
-javac -h . src/LevelDb.java
-g++ -c -fPIC -std=c++11 -I ${JAVA_HOME}/include -I ${JAVA_HOME}/include/darwin -I . LevelDb.cpp -o LevelDb.o
-g++ -dynamiclib -o libleveldbjni.dylib LevelDb.o ./libleveldb.a  -lc
+export set JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home
+export set LEVELDB_DIR=../leveldb
+#javac -h . src/LevelDb.java
+g++ -c -fPIC -std=c++11 -I ${JAVA_HOME}/include -I ${JAVA_HOME}/include/darwin -I ${LEVELDB_DIR}/include LevelDb.cpp -o LevelDb.o
+g++ -dynamiclib -o libleveldbjni.dylib LevelDb.o ${LEVELDB_DIR}/build/libleveldb.a  -lc
