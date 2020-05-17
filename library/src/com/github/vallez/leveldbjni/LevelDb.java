2800afaa0388c4e177f25200706afc03eb40ed80
Valentin Konovalov <valle.ketsujin@gmail.com>
2020-05-17 17:49:19 -0400

    Javadoc

--- library/src/com/github/vallez/leveldbjni/LevelDb.java
+++ library/src/com/github/vallez/leveldbjni/LevelDb.java
@@ -21,9 +21,25 @@ import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Java JNI wrapper for LevelDB.
+ * It loads native JNI wrapper library for appropriate OS and platform and exposes methods
+ * to create, iterate over, add into, delete from a LevelDB database.
+ * <p>
+ * Database should be closed after use to flush data.
+ * <p>
+ * Example:
+ * <p>
+ * try (LevelDb db = new LevelDb(new File("testdb"), new LevelDb.Options())) {
+ * if (db.put("key".getBytes(), "value".getBytes())) {
+ * System.out.println("Read " + new String(db.get("key".getBytes())));
+ * db.delete("key".getBytes());
+ * }
+ * }
+ */
 public class LevelDb implements Closeable {
 
-    public static final String LIBNAME = "leveldbjni";
+    private static final String LIBNAME = "leveldbjni";
 
     static {
         String osName = System.getProperty("os.name", "");
@@ -72,6 +88,12 @@ public class LevelDb implements Closeable {
     private final AtomicBoolean dbClosed = new AtomicBoolean();
     private final ConcurrentHashMap<Iterator, Boolean> iterators = new ConcurrentHashMap<>();
 
+    /**
+     * Opens or creates LevelDB
+     *
+     * @param file    Directory for the DB
+     * @param options db open options
+     */
     public LevelDb(File file, Options options) {
         if (options == null) {
             options = new Options();
@@ -94,24 +116,48 @@ public class LevelDb implements Closeable {
         return dbRef;
     }
 
+    /**
+     *
+     * @return DB iterator
+     */
     public Iterator iterator() {
         Iterator iterator = new Iterator();
         iterators.put(iterator, true);
         return iterator;
     }
 
+    /**
+     *
+     * @return WriteBatch to perform batched writes (commits)
+     */
     public WriteBatch createWriteBatch() {
         return new WriteBatch();
     }
 
+    /**
+     * Delete given key
+     * @param key the key to delete
+     * @return true if successful, false for error
+     */
     public boolean delete(byte[] key) {
         return delete(dbRef, key);
     }
 
+    /**
+     * Get value for given key
+     * @param key the key
+     * @return value
+     */
     public byte[] get(byte[] key) {
         return get(dbRef, key);
     }
 
+    /**
+     * Put key-value pair into the database
+     * @param key key
+     * @param value value
+     * @return true if successful
+     */
     public boolean put(byte[] key, byte[] value) {
         return put(dbRef, key, value);
     }
@@ -160,6 +206,9 @@ public class LevelDb implements Closeable {
 
     private native void close(long dbRef);
 
+    /**
+     * Flush and close database
+     */
     @Override
     public void close() {
         if (dbClosed.compareAndSet(false, true)) {
@@ -170,6 +219,9 @@ public class LevelDb implements Closeable {
         }
     }
 
+    /**
+     * Use WriteBatch to perform atomic writes
+     */
     public class WriteBatch implements Closeable {
         private final long ref;
         private final AtomicBoolean writeBatchClosed = new AtomicBoolean(false);
@@ -181,14 +233,30 @@ public class LevelDb implements Closeable {
             }
         }
 
+        /**
+         * Put operation
+         *
+         * @param key   key
+         * @param value value
+         */
         public void put(byte[] key, byte[] value) {
             writeBatchPut(ref, key, value);
         }
 
+        /**
+         * Delete operation
+         *
+         * @param key key
+         */
         public void delete(byte[] key) {
             writeBatchDelete(ref, key);
         }
 
+        /**
+         * Commits this patch
+         *
+         * @return true if successful
+         */
         public boolean write() {
             if (writeBatchClosed.compareAndSet(false, true)) {
                 return writeBatchWriteAndClose(dbRef, ref);
@@ -196,14 +264,30 @@ public class LevelDb implements Closeable {
             return false;
         }
 
+        /**
+         * Commits this batch
+         */
         public void close() {
             write();
         }
     }
 
+    /**
+     * Iterator over database. Use close() or write() to commit changes.
+     */
     public class Iterator implements Closeable {
         private final long ref;
 
+        /**
+         * Create an iterator. At first it is not pointed to anything.
+         * EX:
+         * try (LevelDb.Iterator it = db.iterator()) {
+         * for (it.seekToFirst(); it.hasNext(); it.next()) {
+         * System.out.println(new String(it.key(), StandardCharsets.UTF_8) +
+         * "=" + new String(it.value(), StandardCharsets.UTF_8));
+         * }
+         * }
+         */
         public Iterator() {
             this.ref = iteratorNew(dbRef);
             if (ref == 0) {
@@ -211,40 +295,69 @@ public class LevelDb implements Closeable {
             }
         }
 
+        /**
+         * Close and free the iterator.
+         */
         public void close() {
             if (iterators.remove(this)) {
                 iteratorClose(ref);
             }
         }
 
+        /**
+         * Seeks to first element
+         */
         public void seekToFirst() {
             iteratorSeekToFirst(ref);
         }
 
+        /**
+         * Is current element valid
+         *
+         * @return true if you can retrieve current element, false if you are out of borders
+         */
         public boolean hasNext() {
             return iteratorValid(ref);
         }
 
+        /**
+         * Proceed forward
+         */
         public void next() {
             iteratorNext(ref);
         }
 
+        /**
+         * @return key of the current element
+         */
         public byte[] key() {
             return iteratorKey(ref);
         }
 
+        /**
+         * @return value of the current element
+         */
         public byte[] value() {
             return iteratorValue(ref);
         }
 
+        /**
+         * Seeks to last element
+         */
         public void seekToLast() {
             iteratorSeekToLast(ref);
         }
 
+        /**
+         * Proceed backward
+         */
         public void prev() {
             iteratorPrev(ref);
         }
 
+        /**
+         * Seeks to the provided key
+         */
         public void seek(byte[] key) {
             iteratorSeek(ref, key);
         }
@@ -263,6 +376,9 @@ public class LevelDb implements Closeable {
         }
     }
 
+    /**
+     * Options for opening existing or creating new database
+     */
     public static final class Options {
         boolean createIfMissing = true;
         boolean errorIfExists;
@@ -274,26 +390,58 @@ public class LevelDb implements Closeable {
         public Options() {
         }
 
+        /**
+         * Create database on open if it is not created yet.
+         * Default value is true.
+         *
+         * @param createIfMissing true to create database, false to throw an error when it is missed
+         */
         public void setCreateIfMissing(boolean createIfMissing) {
             this.createIfMissing = createIfMissing;
         }
 
+        /**
+         * Throw error on open if database already exists.
+         * Default value is false.
+         *
+         * @param errorIfExists true to throw error, false to proceed when database already exists
+         */
         public void setErrorIfExists(boolean errorIfExists) {
             this.errorIfExists = errorIfExists;
         }
 
+        /**
+         * Use Snappy compression when necessary. Default is true.
+         *
+         * @param compression true to use Snappy compression when possible, false to disable it
+         */
         public void setCompression(boolean compression) {
             this.compression = compression;
         }
 
+        /**
+         * Verify integrity on go. Disabled by default.
+         *
+         * @param paranoidChecks true to enable paranoid checks
+         */
         public void setParanoidChecks(boolean paranoidChecks) {
             this.paranoidChecks = paranoidChecks;
         }
 
+        /**
+         * Add cache for DB blocks.
+         *
+         * @param cacheSizeBytes cache size in bytes
+         */
         public void setCacheSizeBytes(long cacheSizeBytes) {
             this.cacheSizeBytes = cacheSizeBytes;
         }
 
+        /**
+         * Use bloom filtering.
+         *
+         * @param bloomFilterBitsPerKey bits per key
+         */
         public void setBloomFilterBitsPerKey(byte bloomFilterBitsPerKey) {
             this.bloomFilterBitsPerKey = bloomFilterBitsPerKey;
         }
