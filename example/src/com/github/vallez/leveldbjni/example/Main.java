4ec6f984714c60471055fa630a3209d2d081827e
Valentin Konovalov <valle.ketsujin@gmail.com>
2020-05-17 17:49:32 -0400

    License

--- example/src/com/github/vallez/leveldbjni/example/Main.java
+++ example/src/com/github/vallez/leveldbjni/example/Main.java
@@ -1,5 +1,21 @@
 package com.github.vallez.leveldbjni.example;
 
+/*
+  Copyright (C) 2020 Valentin Konovalov <valle.ketsujin@gmail.com>
+  <p>
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+  <p>
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+  <p>
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import com.github.vallez.leveldbjni.LevelDb;
 
@@ -8,7 +24,7 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         try (LevelDb db = new LevelDb(new File("testdb"), new LevelDb.Options())) {
-            if (db.put("key".getBytes(), "kgyfekuewncr".getBytes())) {
+            if (db.put("key".getBytes(), "value".getBytes())) {
                 System.out.println("PUT SUCCESS");
                 byte[] value = db.get("key".getBytes());
                 if (value == null) {
@@ -45,6 +61,47 @@ public class Main {
                 }
             }
             System.out.println("write batch");
+            try (LevelDb.WriteBatch writeBatch = db.createWriteBatch()) {
+                writeBatch.delete("c".getBytes());
+                writeBatch.put("b".getBytes(), "b value updated".getBytes());
+            }
+            try (LevelDb.Iterator it = db.iterator()) {
+                for (it.seekToFirst(); it.hasNext(); it.next()) {
+                    byte[] key = it.key();
+                    byte[] value = it.value();
+                    System.out.println(new String(key) + " -> " + new String(value));
+                }
+            }
+        }
+
+
+        try (LevelDb db = new LevelDb(new File("testdb"), new LevelDb.Options())) {
+            if (db.put("key".getBytes(), "value".getBytes())) {
+                System.out.println("Read " + new String(db.get("key".getBytes())));
+                db.delete("key".getBytes());
+            }
+        }
+            db.put("a".getBytes(), "a value".getBytes());
+            db.put("b".getBytes(), "b value".getBytes());
+            db.put("c".getBytes(), "c value".getBytes());
+
+            try (LevelDb.Iterator it = db.iterator()) {
+                for (it.seekToFirst(); it.hasNext(); it.next()) {
+                    byte[] key = it.key();
+                    byte[] value = it.value();
+                    System.out.println(new String(key) + " -> " + new String(value));
+                }
+                for (it.seekToLast(); it.hasNext(); it.prev()) {
+                    byte[] key = it.key();
+                    byte[] value = it.value();
+                    System.out.println(new String(key) + " -> " + new String(value));
+                }
+                for (it.seek("b".getBytes()); it.hasNext(); it.next()) {
+                    byte[] key = it.key();
+                    byte[] value = it.value();
+                    System.out.println(new String(key) + " -> " + new String(value));
+                }
+            }
             try (LevelDb.WriteBatch writeBatch = db.createWriteBatch()) {
                 writeBatch.delete("c".getBytes());
                 writeBatch.put("b".getBytes(), "b value updated".getBytes());
